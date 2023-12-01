84ef37706c21a143c780ea7e324759e8bb3776e2
vitodursopettersson <vitodurso98@gmail.com>
2023-12-01 20:46:54 +0100

    feat: delete message

---
+++ milestone5/main.js
@@ -0,0 +1,251 @@
+'use strict'
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        const currentContacts = 0
+        const newMessage = ''
+        const searchContactKey = ''
+        const contactsFiltered = []
+        const contacts = [
+            {
+                name: 'Michele',
+                avatar: './img/avatar_1.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: './img/avatar_2.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        message: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received',
+                        showDropdown: false
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: './img/avatar_3.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received',
+                        showDropdown: false
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Alessandro B.',
+                avatar: './img/avatar_4.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Si, ma preferirei andare al cinema',
+                        status: 'received',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Alessandro L.',
+                avatar: './img/avatar_5.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ricordati di chiamare la nonna',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Va bene, stasera la sento',
+                        status: 'received',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Claudia',
+                avatar: './img/avatar_6.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao Claudia, hai novità?',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Non ancora',
+                        status: 'received',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'Nessuna nuova, buona nuova',
+                        status: 'sent',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Federico',
+                avatar: './img/avatar_7.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                        status: 'received',
+                        showDropdown: false
+                    }
+                ],
+            },
+            {
+                name: 'Davide',
+                avatar: './img/avatar_8.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                        status: 'received',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                        status: 'sent',
+                        showDropdown: false
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'OK!!',
+                        status: 'received',
+                        showDropdown: false
+                    }
+                ]
+            }
+        ]
+
+        return {
+            contacts,
+            currentContacts,
+            newMessage,
+            searchContactKey,
+            contactsFiltered
+        }
+    },
+    methods: {
+        setCurrentContacts(index) {
+            this.currentContacts = index
+        },
+        sentNewMessage(currentContacts) {
+            this.contacts[currentContacts].messages.push({
+                date: 'now',
+                message: this.newMessage,
+                status: 'sent',
+            })
+            this.newMessage = ''
+            setTimeout((this.sentReply), 1000)
+        },
+        sentReply() {
+            this.contacts[this.currentContacts].messages.push({
+                date: 'now',
+                message: 'Ricevuto 👍',
+                status: 'received',
+            })
+        },
+        searchContact(searchContactKey) {
+            this.contacts.forEach(element => {
+                element.visible = element.name.toLowerCase().includes(searchContactKey.toLowerCase())
+            });
+        },
+        openDropdownMessage(currentContacts, index) {
+            this.contacts[currentContacts].messages[index].showDropdown = true
+            document.addEventListener('click', function (event) {
+
+            })
+        },
+        deleteMessage(currentContacts, index) {
+            this.contacts[currentContacts].messages.splice(index, 1)
+        },
+        deleteMessageForAll(currentContacts, index) {
+            this.contacts[currentContacts].messages[index].message = 'Il messaggio è stato eliminato.'
+        }
+
+
+
+    }
+}).mount('#app')
+
+
+
+
+
+
